73a2ca895d8cbd21f36ecf300e0e2fa370be135b
edxon20 <edixon20pes@gmail.com>
2023-06-13 12:11:37 -0400

    [Finalize] function insertAtFront into linkedlist

--- linkedLIst/main.cpp
+++ linkedLIst/main.cpp
@@ -17,6 +17,20 @@ void printList(Node *n) {
     }    
 }
 
+void insertAtFront(Node**head, int newValue) {
+
+    //1. Prepare a newNode
+    Node* newNode = new Node();
+    newNode->Value = newValue;
+    //2. Put it in front of current head
+    newNode->Next = *head;
+    //3. MOve head of the list to point to the newNode
+    *head = newNode;
+
+
+
+}
+
 int main () {
 //firts element
 Node* head = new Node();
@@ -32,6 +46,11 @@ second->Next = third;
 third->Value = 4;
 third->Next = NULL;
 
+cout<<"LinkedList standar: "<<endl;
+printList(head);
+cout<<endl;
+insertAtFront(&head, -22);
+cout<<"LinkedList add a new value in front: "<<endl;
 printList(head);
 
 return 0;
