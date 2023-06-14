8dd08d16baaf8903432d3e5cd41cbb34afc07f70
edxon20 <edixon20pes@gmail.com>
2023-06-14 17:29:32 -0400

    [ADD] print into avoid pointer

--- Pointers/avoid pointer/main.cpp
+++ Pointers/avoid pointer/main.cpp
@@ -9,14 +9,25 @@ using namespace std;
         cout<<*charPtr<<endl;
     }
 
+    void print(void* ptr, char type ){
+        switch(type){
+            case 'i': cout<< *((int*)ptr) <<endl; break;
+            case 'c': cout<< *((char*)ptr) <<endl; break;
+        }        
+    }
+
 
 int main () {
 
    int number = 5;
     char letter = 'a';
 
-    printNumber(&number);
-    printLetter(&letter);
+    // printNumber(&number);
+    // printLetter(&letter);
+    cout<<&number<<endl;
+
+    print(&number, 'i');
+    print(&letter, 'c');
 
 
     return 0;
