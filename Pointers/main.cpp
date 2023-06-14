f0c0d311d00449cf3351974b071380551d72b5cd
edxon20 <edixon20pes@gmail.com>
2023-06-14 16:53:24 -0400

    [Add] basic pointer 2

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
-
-
 using namespace std;
-
-
 
 int main () {
 
     //PTR
 
     // direction --- direction to appoint
+    // The principal variable must be same type to pointer. int -- int not int -- float or reverse.
 
     int n = 5;
     cout<< &n << endl;
 
-    int* prt = &n;
-    cout<< &prt << endl;
-    cout<< *prt << endl;
+    int* ptr = &n;
+    cout<< ptr << endl;
+    cout<< *ptr << endl;
+
+    *ptr = 10;
+
+    cout<< *ptr <<endl;    
+    cout<< n <<endl;    
+
+    //Error
+    // int* ptr2;
+    // *ptr2 = 7;
+
+    int v;
+    int* ptr2 = &v;
+    *ptr2 = 3;
+    cout <<"v: " << v << endl;
 
 
 
