66d68a1d6a7f1514782561c8c854c83b1e3170dc
edxon20 <edixon20pes@gmail.com>
2023-06-14 16:45:04 -0400

    Pointer base

---
+++ Pointers/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+
+int main () {
+
+    //PTR
+
+    // direction --- direction to appoint
+
+    int n = 5;
+    cout<< &n << endl;
+
+    int* prt = &n;
+    cout<< &prt << endl;
+    cout<< *prt << endl;
+
+
+
+
+    return 0;
+}
