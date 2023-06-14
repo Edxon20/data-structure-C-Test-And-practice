1b35c1c1407f079a6e4db3d8a33e69d0dbb027c8
edxon20 <edixon20pes@gmail.com>
2023-06-14 18:23:15 -0400

    [ADD] minANdMax with funcction than modify from memory the values

---
+++ Pointers/arrays with pointers/minAndMax/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void getMinAndMax(int numbers [], int size, int* min, int* max){
+
+    for (int i = 0; i < size; i++)
+    {
+        if(numbers[i] > *max)
+            *max = numbers[i];
+        
+
+        if(numbers[i] < *min)
+            *min = numbers[i];
+        
+    }          
+  
+}
+
+int main () {
+
+    int numbers[5] = {4,5,6,8,-12};
+    
+    int min = numbers[0];
+    int max = numbers[0];
+
+    getMinAndMax(numbers,5,&min,&max);
+
+    cout<<"Min: "<<min<<endl;
+    cout<<"Max: "<<max<<endl;
+
+    return 0;
+}
